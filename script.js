b1305f8fc4e4c9cd0f43513315a429a86fc2adcb
BanalaMery <mery913313@gmail.com>
2026-05-16 10:30:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-async function getWeather() {
+async function getWeather(){
 
-    const city = document.getElementById("city").value;
+const city =
+document.getElementById("city").value;
+const apiKey = "afb1f33923571eeb0acd50f1b0a7d5f3";
 
-    const apiKey = "afb1f33923571eeb0acd50f1b0a7d5f3";
+const url =
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    const url =
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+const response = await fetch(url);
 
-    const response = await fetch(url);
+const data = await response.json();
 
-    const data = await response.json();
-    
-    document.getElementById("result").innerHTML = `
-        <h2>${data.name}</h2>
-        <h3>${data.main.temp} C</h3>
-        <p>${data.weather[0].description}</p>
-        <p>Humidity: ${data.main.humidity}%</p>
-        <p>Wind Speed: ${data.wind.speed} m/s</p>
-     `;
-    if (data.cod !== 200) {
-    document.getElementById("result").innerHTML = "❌ City not found";
-    return;
-}
+document.getElementById("result")
+.innerHTML = `
+
+<h2>${data.name}</h2>
+
+<img src=
+"https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+
+<h3>${data.main.temp} °C</h3>
+
+<p>${data.weather[0].description}</p>
+
+<p>Humidity: ${data.main.humidity}%</p>
+
+<p>Wind Speed: ${data.wind.speed} m/s</p>
+
+`;
+
 }
