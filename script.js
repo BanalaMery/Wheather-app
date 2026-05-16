0fec8f01dc1af5b021045ed9bf37b1688798cb27
BanalaMery <mery913313@gmail.com>
2026-05-16 12:04:47 +0530

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ async function getWeather(){
 const city =
 document.getElementById("city").value;
 
-const apiKey = "YOUR_API_KEY";
+const apiKey = "afb1f33923571eeb0acd50f1b0a7d5f3";
+
 
 const url =
 `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
